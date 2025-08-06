3fd90d68127fb95a41f5bf8bb3eceb13d452f6ab
denborum <denborum@water.com>
2025-08-05 22:24:47 -0300

    v3.0.1: - Added method to bypass login auth via NODE_MODE environment variable (only for development).

--- src/middleware/loginAuth.ts
+++ src/middleware/loginAuth.ts
@@ -4,6 +4,10 @@ export function authLogin(req: Request, res: Response, next: NextFunction) {
   if (req.session && req.session.user) {
     next()
   } else {
-    res.status(401).json({message: 'Access forbidden.'})
+    if (process.env.NODE_MODE && process.env.NODE_MODE === 'dev') {
+      next()
+    } else {
+      res.status(401).json({message: 'Access forbidden.'})
+    }
   }
 }
