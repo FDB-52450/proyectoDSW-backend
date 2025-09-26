d4bf9576ef1cdbfef5407e0424aca3141aeaf3ea
denborum <denborum@water.com>
2025-09-26 02:05:42 -0300

    v4.6.2: - Updated validation for Administrador endpoints to fix various bugs related to update operations not working as intended.

--- src/administrador/administrador-routes.ts
+++ src/administrador/administrador-routes.ts
@@ -10,13 +10,13 @@ import { handleValidation } from '../middleware/validation/validateInput.js'
 
 export const administradorRouter = Router()
 
-administradorRouter.post('/login', validateAdmin, handleValidation, login)
+administradorRouter.post('/login', validateAdmin(), handleValidation, login)
 administradorRouter.get('/logout', authLogin, logout)
 administradorRouter.get('/me', authLogin, me)
 
 administradorRouter.get('/', superAuthLogin, findAll)
 administradorRouter.get('/:id', superAuthLogin, findOne)
-administradorRouter.post('/', superAuthLogin, validateAdmin, handleValidation, add)
-administradorRouter.put('/:id', superAuthLogin, validateId, validateAdmin, handleValidation, update)
-administradorRouter.patch('/:id', superAuthLogin, validateId, validateAdmin, handleValidation, update)
+administradorRouter.post('/', superAuthLogin, validateAdmin(), handleValidation, add)
+administradorRouter.put('/:id', superAuthLogin, validateId, validateAdmin('update'), handleValidation, update)
+administradorRouter.patch('/:id', superAuthLogin, validateId, validateAdmin('update'), handleValidation, update)
 administradorRouter.delete('/:id', superAuthLogin, validateId, handleValidation, remove)
--- src/middleware/validation/adminValidation.ts
+++ src/middleware/validation/adminValidation.ts
@@ -1,25 +1,54 @@
 import { body } from 'express-validator';
 
-export const validateAdmin = [
-  body('nombre')
-    .trim()
-    .notEmpty().withMessage('El nombre es obligatorio').bail()
-    .isLength({ max: 50 }).withMessage('El nombre no puede tener más de 50 caracteres'),
+export function validateAdmin(mode = "create") {
+    const isUpdate = (mode === "update")
 
-  body('password')
-    .trim()
-    .hide('******')
-    .notEmpty().withMessage('La contraseña es obligatoria').bail()
-    .isLength({ max: 50 }).withMessage('La contraseña no puede tener más de 50 caracteres'),
-  
-  body().custom(body => {
-    const allowed = ['nombre', 'password']
-    const extraKeys = Object.keys(body).filter(key => !allowed.includes(key))
+    const validations = [
+        body('nombre')
+            .if((value, { req }) => {return !isUpdate || req.body.nombre !== undefined})
+            .trim()
+            .notEmpty().withMessage('El nombre es obligatorio').bail()
+            .isLength({ max: 50 }).withMessage('El nombre no puede tener más de 50 caracteres'),
 
-    if (extraKeys.length) {
-      throw new Error(`Campos no válidos: ${extraKeys.join(', ')}`)
+        body('password')
+            .if((value, { req }) => {return !isUpdate || req.body.password !== undefined})
+            .trim()
+            .hide('******')
+            .notEmpty().withMessage('La contraseña es obligatoria').bail()
+            .isLength({ max: 50 }).withMessage('La contraseña no puede tener más de 50 caracteres'),
+        
+        body().custom(body => {
+            const allowed = ['nombre', 'password']
+            const extraKeys = Object.keys(body).filter(key => !allowed.includes(key))
+
+            if (extraKeys.length) {
+                throw new Error(`Campos no válidos: ${extraKeys.join(', ')}`)
+            }
+
+            return true
+        })
+    ]
+ 
+    if (isUpdate) {
+        validations.push(
+            body()
+            .custom(body => {
+                const allowedFields = ['nombre', 'password']
+
+                if (!body || typeof body !== 'object') {
+                    throw new Error('Debe proporcionar al menos un campo para actualizar')
+                }
+
+                const hasValidField = Object.keys(body).some(key => allowedFields.includes(key))
+                
+                if (!hasValidField) {
+                    throw new Error('Debe proporcionar al menos un campo para actualizar')
+                }
+                    
+                return true;
+            })
+        );
     }
 
-    return true
-  })
-]
+    return validations
+}
