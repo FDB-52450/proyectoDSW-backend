4dfc6d74aff1abd1b13eee57b6f1a252cceb8d48
denborum <denborum@water.com>
2025-10-14 22:50:13 -0300

    v5.2.1: - Added images for all marca entities used for seeding. - Moved images used for marcas to new /brandImages folder. - Implemented automatic deletion of old images when starting new seed process.

--- seeders/clearSeeds.ts
+++ seeders/clearSeeds.ts
@@ -1,3 +1,6 @@
+import path from "path"
+import fs from 'fs'
+
 import { Administrador } from "../src/administrador/administrador-entity.js";
 import { Categoria } from "../src/categoria/categoria-entity.js";
 import { Cliente } from "../src/cliente/cliente-entity.js";
@@ -6,7 +9,25 @@ import { Pedido } from "../src/pedido/pedido-entity.js";
 import { PedidoProd } from "../src/pedidoprod/pedidoprod-entity.js";
 import { Producto } from "../src/producto/producto-entity.js";
 
-import { MikroORM } from "@mikro-orm/mysql";
+import { MikroORM } from "@mikro-orm/mysql"
+
+function clearImages() {
+    const folderPath = path.resolve('images')
+    const normalizedPath = path.normalize(folderPath)
+    const lastPart = path.basename(normalizedPath)
+
+    // Check to prevent wrong folder from being deleted.
+    if (lastPart !== 'images') {
+        return
+    }
+
+    try {
+        fs.rmSync(folderPath, {recursive: true, force: true})
+        fs.mkdirSync(folderPath, {recursive: true})
+    } catch (err) {
+        console.error('Error borrando imagenes', err)
+    }
+}
 
 export async function clearSeeds(orm: MikroORM) {
     const clearingEm = orm.em.fork()
@@ -19,6 +40,8 @@ export async function clearSeeds(orm: MikroORM) {
     await clearingEm.nativeDelete(Marca, {})
     await clearingEm.nativeDelete(Categoria, {})
     await clearingEm.nativeDelete(Administrador, {})
+
+    clearImages()
 
     for (const tableName of tables) {
         await clearingEm.getConnection().execute(`ALTER TABLE ${tableName} AUTO_INCREMENT = 1`);
--- seeders/seeds/seedMarcas.ts
+++ seeders/seeds/seedMarcas.ts
@@ -1,6 +1,28 @@
-import { Marca } from "../../src/marca/marca-entity.js"
+import path from "path"
+import fs from 'fs'
 
 import { MikroORM } from "@mikro-orm/mysql"
+import { fileURLToPath } from "url"
+
+import { Marca } from "../../src/marca/marca-entity.js"
+import { Imagen } from "../../src/imagen/imagen-entity.js"
+
+function getFileBufferIfExists(productName: string): Buffer | null {
+    const productFilename = productName.toLowerCase().replace(' ', '') + 'Logo.webp'
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const imagePath = path.join(__dirname, '..', '..', '..', 'tests', 'testImages', 'brandImages', productFilename)
+
+    try {
+        if (fs.existsSync(imagePath)) {
+            return fs.readFileSync(imagePath)
+        } else {
+            return null
+        }
+    } catch (err) {
+        return null
+    }
+}
 
 export async function seedMarcas(orm: MikroORM) {
     const marcaEm = orm.em.fork()
@@ -14,7 +36,10 @@ export async function seedMarcas(orm: MikroORM) {
     ]
 
     for (const nombre of nombresBase) {
-        const marca = new Marca(nombre, null)
+        const imgBuffer = getFileBufferIfExists(nombre)
+        const imagen = imgBuffer ? new Imagen(imgBuffer, true) : null
+
+        const marca = new Marca(nombre, imagen)
 
         marcas.push(marca);
         marcaEm.persist(marca);
